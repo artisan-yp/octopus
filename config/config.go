2c9bec8632bc1097798274ee5c77eff05e2c13bb
artisan.yp <artisan.yp@gmail.com>
2021-03-19 12:36:14 +0800

    Add config cast function.

--- config/config.go
+++ config/config.go
@@ -2,6 +2,9 @@ package config
 
 import (
 	"log"
+	"time"
+
+	"github.com/k8s-practice/octopus/utils/cast"
 	// NOTE: Import these packages while using them, for reducing program size.
 	// To register jsonparser
 	// _ "github.com/k8s-practice/octopus/config/parser/jsonparser"
@@ -46,4 +49,64 @@ func New(targets ...Target) Config {
 	}
 
 	return c
+}
+
+func Get(c Config, key string) interface{} {
+	return c.Get(key)
+}
+
+func GetBool(c Config, key string) bool {
+	return cast.ToBool(c.Get(key))
+}
+
+func GetInt(c Config, key string) int {
+	return cast.ToInt(c.Get(key))
+}
+
+func GetInt32(c Config, key string) int32 {
+	return cast.ToInt32(c.Get(key))
+}
+
+func GetInt64(c Config, key string) int64 {
+	return cast.ToInt64(c.Get(key))
+}
+
+func GetIntSlice(c Config, key string) []int {
+	return cast.ToIntSlice(c.Get(key))
+}
+
+func GetUint(c Config, key string) uint {
+	return cast.ToUint(c.Get(key))
+}
+
+func GetUint32(c Config, key string) uint32 {
+	return cast.ToUint32(c.Get(key))
+}
+
+func GetUint64(c Config, key string) uint64 {
+	return cast.ToUint64(c.Get(key))
+}
+
+func GetFloat32(c Config, key string) float32 {
+	return cast.ToFloat32(c.Get(key))
+}
+
+func GetFloat64(c Config, key string) float64 {
+	return cast.ToFloat64(c.Get(key))
+}
+
+func GetString(c Config, key string) string {
+	return cast.ToString(c.Get(key))
+}
+
+func GetStringSlice(c Config, key string) []string {
+	return cast.ToStringSlice(c.Get(key))
+}
+
+func GetTime(c Config, key string) time.Time {
+	return cast.ToTime(c.Get(key))
+}
+
+func GetDuration(c Config, key string) time.Duration {
+	return cast.ToDuration(c.Get(key))
 }
--- test/config/config_test.go
+++ test/config/config_test.go
@@ -3,6 +3,7 @@ package test
 import (
 	"log"
 	"testing"
+	"time"
 
 	// NOTE: Import these packages while using them, for reducing program size.
 	"github.com/k8s-practice/octopus/config"
@@ -34,17 +35,20 @@ func TestNewConfig(t *testing.T) {
 			WithFormat(jsonparser.Format()),
 	)
 
-	assert.Equal(t, c.Get("database.addr"), "172.168.0.1",
+	assert.Equal(t, config.GetString(c, "database.addr"), "172.168.0.1",
 		"p1 database.addr should be 172.168.0.1")
 
 	c.Set("database.addr", "127.0.0.1")
-	assert.Equal(t, c.Get("database.addr"), "127.0.0.1",
+	assert.Equal(t, config.GetString(c, "database.addr"), "127.0.0.1",
 		"after set, database.addr should be 127.0.0.1")
 
 	c.Set("database.addr", nil)
-	assert.Equal(t, c.Get("database.addr"), "172.168.0.1",
+	assert.Equal(t, config.GetString(c, "database.addr"), "172.168.0.1",
 		"after delete, database.addr should be 172.168.0.1")
 
-	assert.Equal(t, c.Get("database.port"), 3307,
+	assert.Equal(t, config.GetInt(c, "database.port"), 3307,
 		"p2 database.port should be 3307")
+
+	assert.True(t, true, config.GetTime(c, "database.time").
+		Equal(time.Date(2021, time.March, 3, 19, 14, 15, 16, time.UTC)))
 }
