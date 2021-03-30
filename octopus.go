1633eeb95f707a75d2f82dd2a47f52758c23aadd
artisan.yp <artisan.yp@gmail.com>
2021-03-30 14:35:24 +0800

    octopus add Load function.

--- octopus.go
+++ octopus.go
@@ -2,14 +2,21 @@ package octopus
 
 import (
 	"github.com/k8s-practice/octopus/config"
+	"github.com/mitchellh/mapstructure"
 )
 
 func New() *Octopus {
-	return &Octopus{}
+	return &Octopus{
+		frameInit: make([]func() error, 0),
+		appInit:   make([]func() error, 0),
+	}
+
 }
 
 // Brain is the brain of octopus.
 type Octopus struct {
+	frameLogger interface{}
+
 	conf config.Config
 
 	// frameInit is the framework initialize functions slice.
@@ -21,8 +28,15 @@ type Octopus struct {
 	appInit []func() error
 }
 
+// WithConfig sets Octopus.conf .
 func (o *Octopus) WithConfig(c config.Config) *Octopus {
 	o.conf = c
 
 	return o
+}
+
+// Load loads configuration by key from Octopus.conf .
+func (o *Octopus) Load(key string, i interface{}) error {
+	err := mapstructure.Decode(o.conf.Get(key), i)
+	return err
 }
