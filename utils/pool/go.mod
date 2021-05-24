cadb456233e34cd68af0ab15c4b792c792d93ad9
ZT-Charlotte <zhangting01@sunlands.com>
2021-05-24 15:43:17 +0800

    task pool

---
+++ utils/pool/go.mod
@@ -0,0 +1,11 @@
+module go.task.pool
+
+go 1.14
+
+replace go.common.errno => ../errno/
+
+require (
+	github.com/artisan-yp/go-rotatablezap v1.0.3
+	go.uber.org/zap v1.16.0
+	go.common.errno v0.0.0-00010101000000-000000000000
+)
---
+++ utils/pool/task_pool.go
@@ -0,0 +1,86 @@
+package pool
+
+import (
+	"sync"
+	"time"
+
+	"github.com/artisan-yp/go-rotatablezap"
+	"go.uber.org/zap"
+
+	errno "go.common.errno"
+)
+
+var sugar *zap.SugaredLogger
+
+func init() {
+	sugar = rotatablezap.New("TaskPool").Sugar()
+}
+
+func SetLoggerHandler(logger *zap.SugaredLogger) {
+	if logger != nil {
+		sugar = logger
+	}
+}
+
+type Work interface {
+	Run() error
+}
+
+type Pool struct {
+	work chan Work
+	wg   sync.WaitGroup
+	sema chan struct{}
+}
+
+func (pool *Pool) Run(work Work) {
+	pool.work <- work
+}
+
+func (pool *Pool) RunWithTimeOut(work Work, t time.Duration) error {
+	poolDelay := time.NewTimer(t)
+	defer poolDelay.Stop()
+
+	select {
+	case pool.work <- work:
+		return nil
+	case <-poolDelay.C:
+		sugar.Infof("timeout ...")
+		return errno.DealTimeoutErr
+	}
+}
+
+func (pool *Pool) Shutdown() {
+	pool.sema <- struct{}{}
+	pool.wg.Wait()
+	close(pool.work)
+	close(pool.sema)
+}
+
+func New(maxGoroutines, chanSize int) *Pool {
+	sugar.Infof("pool: goroutines %d, chanSize: %d", maxGoroutines, chanSize)
+
+	pool := Pool{
+		work: make(chan Work, chanSize),
+		sema: make(chan struct{}, 1),
+	}
+
+	pool.wg.Add(maxGoroutines)
+	for i := 0; i < maxGoroutines; i++ {
+		go func() {
+			for {
+				select {
+				case <-pool.sema:
+					goto Done
+				case work := <-pool.work:
+					work.Run()
+				}
+			}
+
+		Done:
+			pool.sema <- struct{}{}
+			pool.wg.Done()
+		}()
+	}
+
+	return &pool
+}
